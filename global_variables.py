95189a6bbe73e91303b8bb1c7a29a512cf8dea18
TheoSanto <matteo.santini7@studio.unibo.it>
2022-04-08 16:02:26 +0000

    Upload New File

---
+++ global_variables.py
@@ -0,0 +1,95 @@
+from multiprocessing.connection import wait
+from pickle import TRUE
+from matplotlib import colors
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random.mtrand import rand
+from matplotlib import cm
+from matplotlib import animation
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+from matplotlib.pyplot import figure
+import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Inizialization of the Ambient
+global dimension 
+global npeople
+global nwhite
+global nred 
+global norange
+global ncyan 
+global nblue 
+global ambient_evolution
+global nsteps
+global ax
+global distance
+
+# Animation Settings
+nsteps = 100
+interval = 200 #ms
+
+# Ambient Variables
+dimension = 961 ### side è 11
+side = int(np.sqrt(dimension)) # 31
+ambient = [-3]*dimension
+ambient_evolution = [ambient]*(nsteps)
+
+# Initial Conditions
+nwhite = 0
+nred = 0
+norange = 0
+ncyan = 0
+nblue = 0
+
+# Scenario Choice
+setup = 1
+# 0 per Scenario di Distanza, 
+# 1 per Scenario di Flocking Gravitazionale,
+# 2 per Scenario di Visione Parziale.  
+
+# Control Parameters
+npeople = 100  ### fundamental
+distance = 4  # MAX = int((side-1)/2)
+G = 10 
+neutral_prob = 0.55
+
+'''
+MODELLO DI ISING: here 'https://it.abcdef.wiki/wiki/Ising_model'
+
+OBIETTIVI
+1) Dobbiamo controllare il comportamento del sistema al variare del parametro del controllo DISTANCE
+ossia vedere se si raggiunge o meno il consenso a parità di densità popolativa media(=npeople/dimension)
+
+2) Una volta fatto ciò, possiamo introdurre un nuvo parametro di controllo, ossia il numero costante e limitato di amici,
+da cui l'individuo viene influenzato ad ogni istante temporale
+
+3) Provare ad implementare una memoria del sistema (mi sembra infattibile e molto impegnativo computazionalmente)
+
+4) Provare ad implementare l'attrazione simil-gravitazionale tra individui con opinione concorde
+   (la vedo già più fattibile, ci si può lavorare)
+
+5) Inserire un campo esterno rappresentativo dei media (come già fatto, ma magari implementare due casi:
+   uno dove le variazioni casuali dei media sono pseudo-continue, ossia piccole entro una certa tolleranza 
+   scelta dall'utente, ed un'altro dove ad un certo istante il campo esterno subisce una o più variazione 
+   improvvisa e significativa, per vedere se il sistema riesce a trovare un equilibrio comunque o meno)
+
+
+DUBBI
+1) Gli individui ai bordi sono influenzati anche dagli indidui dall'altra parte dell'ambiente??
+
+2) Come implementare le probabilità di transizione tra le possibili opinioni (-2,-1,0,1,2) ??
+
+3) In qualche modo forse il parametro di distanza è correlato alle fluttuazioni del sistema 
+   che in teoria sono rappresentate fisicamente dal parametro di temperatura. Infatti, m aspetto 
+   che maggiore è la distanza, maggiore è la temperature; ma allo stesso tempo anche che 
+   minore è il numero limitato di amici che influenzano l individuo, minore è la temperatura.
+   Ha senso??
+
+4) Non so quanto senso abbia implementare tutte queste cose contemporaneamente. Proporrei di fare 
+   una cosa alla volta tenendo le altre 'fissate', nel senso che:
+   ad esempio, quando si intende studiare il comportamento del sistema per diversi valori di distanza,
+   direi di annullare sia l'effetto del campo esterno dei media che dell'attrazione semi-gravitazionale
+   e poi per andare a svolgere gli obiettivi successivi si può scegliere un valore fisso di distanza 
+   per cui sappiamo sia possibile il raggiungimento del consenso
+'''
