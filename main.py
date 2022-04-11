b939513bee36c35bc5916e38d836cc714ef1242a
Matteo Costa <matty24.mc@gmail.com>
2022-04-11 09:23:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,59 @@ glob.np.random.seed(196808822)
 
 # Random Positioning & Opinion Defining of the Individuals
 if glob.fixed_or_random==0 :
-    print('yes')
     glob.ambient_evolution[0] = f.init_random()
 if glob.fixed_or_random==1 :
-    print('no')
     glob.ambient_evolution[0] = f.init_fixed()
 
 # Implementation and Storage of Time Evolution of 1° Scenario
 if glob.setup==0 :
-    for t in range(0, glob.nsteps) : 
-                print("Step", t, ', Mode',glob.setup)
-                if t>0 :
-                    glob.ambient_evolution[t] = f.evolve_norm(glob.ambient_evolution[t-1])
-    time_data = f.time_distribution(glob.ambient_evolution)
-    fig1 = glob.plt.figure()
-    ax1 = fig1.add_axes([0.1, 0.1, 0.5, 0.75])
-    ax1.hist(time_data)
-    glob.plt.show()
+    # lista di array 2D per lo storage degli effetti prodotti da condizioni iniziali diverse
+    initial_conditions = []
+    final_conditions = []
+    for n in range(glob.nsimulations) :
+        print("Simulation n°", n)
+
+        if glob.fixed_or_random==0 :
+            glob.ambient_evolution[0] = f.init_random()
+        if glob.fixed_or_random==1 :
+            glob.ambient_evolution[0] = f.init_fixed()
+        # utile per Magnetization Analysis
+        #magnetization_data = []
+        #spin_data_0 = f.count_all(glob.ambient_evolution[0])
+        #magnetization_data.append((1/glob.npeople)*(spin_data_0[1]-spin_data_0[0]))
+
+        for t in range(0, glob.nsteps) : 
+                    #print("Step", t, ', Mode',glob.setup)
+                    if t>0 :
+                        glob.ambient_evolution[t] = f.evolve_norm(glob.ambient_evolution[t-1])
+
+                        #spin_data_t = f.count_all(glob.ambient_evolution[t])
+                        #magnetization_data.append((1/glob.npeople)*(spin_data_t[1]-spin_data_t[0]))
+
+        #times = glob.np.linspace(0,glob.nsteps,glob.nsteps)
+        #glob.plt.plot(times,magnetization_data)
+        #glob.plt.show()
+
+        # utile per Time Distribution Analysis
+        time_data = f.time_distribution(glob.ambient_evolution)
+        fig1 = glob.plt.figure()
+        ax1 = fig1.add_axes([0.1, 0.1, 0.5, 0.75])
+        ax1.hist(time_data,bins=int(glob.nsteps))
+        glob.plt.yscale('log')
+        glob.plt.xscale('log')
+        glob.plt.show()
+        print('\n\n')
+
+        # utile per Initial Conditions Analysis
+        #nth_opinion_data_in = f.count_all(glob.ambient_evolution[0])
+        #nth_opinion_data_fin = f.count_all(glob.ambient_evolution[glob.nsteps-1])
+        #initial_conditions.append(nth_opinion_data_in)
+        #final_conditions.append(nth_opinion_data_fin)
+    #prob_blue = (final_conditions.count([0, glob.npeople]))/(glob.nsimulations)
+    #prob_red = (final_conditions.count([glob.npeople, 0]))/(glob.nsimulations)
+    #assert (len(initial_conditions)==glob.nsimulations) and (len(final_conditions)==glob.nsimulations), 'ATTENTION: Something wrong about the Storage of Simulations\' initial and final conditions.'
+    #print(initial_conditions,'\n\n',final_conditions)
+    #print(prob_blue, 'of Blue &', prob_red, 'of Red.')
 
 # Implementation and Storage of Time Evolution of 2° Scenario
 if glob.setup==1 :
@@ -54,7 +90,7 @@ if glob.setup==2 :
 # Implementaion of Cellular Automata Visualization and Animation
 fig = glob.plt.figure()
 glob.ax = fig.add_axes([0.1, 0.1, 0.5, 0.75])
-glob.ax_histo = fig.add_axes([0.65, 0.1, 0.3, 0.4])
+glob.ax_histo = fig.add_axes([0.7, 0.1, 0.23, 0.4])
 anim = glob.animation.FuncAnimation(fig, f.update_scatter, frames=glob.nsteps, interval=glob.interval)
 anim.save('simulation_try.gif')
 #glob.plt.show() 
