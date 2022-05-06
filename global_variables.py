aed8d42d6708d72bf2ab3fb7e4d3fd4869d383af
TheoSanto <65360581+TheoSanto@users.noreply.github.com>
2022-05-06 20:04:56 +0200

    Update global_variables.py

--- global_variables.py
+++ global_variables.py
@@ -21,7 +21,7 @@ global ax_histo
 
 
 # Animation Settings #########################################################
-nsteps = 100000
+nsteps = 300
 interval = 60 #ms
 
 
@@ -33,7 +33,7 @@ ambient_evolution = [ambient]*(nsteps)
 
 
 # Scenario Choice ############################################################
-setup = 0
+setup = 2
 # 0 per Scenario di Distanza, 
 # 1 per Scenario di Flocking Gravitazionale,
 # 2 per Scenario di Visione Parziale. 
@@ -56,23 +56,18 @@ init_analysis = False
 # False if Initial Conditions Analysis is not wanted
 # True if Initial Conditions Analysis is wanted
 
-balancing = True
+balancing = False
 # False if Stationary Condition Analysis is not wanted
 # True if Stationary Condition Analysis is wanted (recommended)
 
 
 # Control Parameters #######################################################
 npeople = 100
-distance = 1
+distance = 4
 G = 10
 T = 1
-vision = 2
+vision = 8
 initial_reds = 50
-
-
-# Benchmark Variables ######################################################
-time_influence_norm = 0
-time_empty_spaces = 0
 
 
 # Tests of Initialization's Consistence ####################################
@@ -85,43 +80,3 @@ assert (distance>0) and (distance<=int((side-1)/2)), 'ATTENTION: Negative or too
 assert G > 0, 'ATTENTION: Negative Universal Gravitational Constant G.'
 assert T > 0, 'ATTENTION: Negative Temperature T.'
 assert (vision>0) and (vision<npeople), 'ATTENTION: Negative or too elevated vision Value.'
-
-'''
-MODELLO DI ISING: here 'https://it.abcdef.wiki/wiki/Ising_model'
-
-OBIETTIVI
-1) Dobbiamo controllare il comportamento del sistema al variare del parametro del controllo DISTANCE
-ossia vedere se si raggiunge o meno il consenso a parità di densità popolativa media(=npeople/dimension)
-
-2) Una volta fatto ciò, possiamo introdurre un nuvo parametro di controllo, ossia il numero costante e limitato di amici,
-da cui l'individuo viene influenzato ad ogni istante temporale
-
-3) Provare ad implementare una memoria del sistema (mi sembra infattibile e molto impegnativo computazionalmente)
-
-4) Provare ad implementare l'attrazione simil-gravitazionale tra individui con opinione concorde
-   (la vedo già più fattibile, ci si può lavorare)
-
-5) Inserire un campo esterno rappresentativo dei media (come già fatto, ma magari implementare due casi:
-   uno dove le variazioni casuali dei media sono pseudo-continue, ossia piccole entro una certa tolleranza 
-   scelta dall'utente, ed un'altro dove ad un certo istante il campo esterno subisce una o più variazione 
-   improvvisa e significativa, per vedere se il sistema riesce a trovare un equilibrio comunque o meno)
-
-
-DUBBI
-1) Gli individui ai bordi sono influenzati anche dagli indidui dall'altra parte dell'ambiente??
-
-2) Come implementare le probabilità di transizione tra le possibili opinioni (-2,-1,0,1,2) ??
-
-3) In qualche modo forse il parametro di distanza è correlato alle fluttuazioni del sistema 
-   che in teoria sono rappresentate fisicamente dal parametro di temperatura. Infatti, m aspetto 
-   che maggiore è la distanza, maggiore è la temperature; ma allo stesso tempo anche che 
-   minore è il numero limitato di amici che influenzano l individuo, minore è la temperatura.
-   Ha senso??
-
-4) Non so quanto senso abbia implementare tutte queste cose contemporaneamente. Proporrei di fare 
-   una cosa alla volta tenendo le altre 'fissate', nel senso che:
-   ad esempio, quando si intende studiare il comportamento del sistema per diversi valori di distanza,
-   direi di annullare sia l'effetto del campo esterno dei media che dell'attrazione semi-gravitazionale
-   e poi per andare a svolgere gli obiettivi successivi si può scegliere un valore fisso di distanza 
-   per cui sappiamo sia possibile il raggiungimento del consenso
-'''
